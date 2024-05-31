751300faa2073a8504c7878113d2a8d54868aa49
Nathanaelkit <nathanaelkitowijaya@gmail.com>
2024-05-31 22:56:14 +0700

    Update settingpage.dart

---
+++ stockin/lib/pages/settingpage.dart
@@ -0,0 +1,167 @@
+
+
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class settingPage extends StatefulWidget{
+  const settingPage({super.key});
+
+  @override
+  State <settingPage> createState() => settingPageState();
+}
+
+class settingPageState extends State <settingPage>{
+
+
+Future <void> logoutHandle () async{
+
+
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  Scaffold(
+        backgroundColor: Color(0xff2B314A),
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+            child: Text(
+              "Manajemen Stok",
+              style: TextStyle(
+                  color: Color(0xff41B06E), fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0, top: 10.0),
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          items: const [
+            Icon(
+              Icons.home_rounded,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.add_box,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.monetization_on_rounded,
+              color: Colors.white,
+            ),
+          ],
+          color: Color(0xff41B06E),
+          backgroundColor: Colors.white,
+        ),
+        body:Column(
+          
+            children: [
+              const SizedBox(height:50.0),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Settings', 
+                      style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      ),
+                    ),
+                  )
+                ),
+              
+              const SizedBox(height:10.0),
+                Expanded(
+
+                child: Container(
+                decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0))
+                ),
+                width: double.infinity,
+                child : Column(
+                children: [
+
+                const SizedBox(height:20.0),
+                Padding( 
+                padding:  EdgeInsets.all(16.0),                
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.grey[300],
+                      child: Icon(
+                        Icons.person,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Nathanael Kito',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Owner',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(220,40),
+                                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 152.0),
+                                backgroundColor: Color(0xff41B06E),
+                                foregroundColor: Color(0xffFFFFFF),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),)
+                                  ),
+                              onPressed: logoutHandle,
+                              child: Text("Profile", style: TextStyle(fontSize: 20.0, color: Color(0xffFFFFFF)),),
+                                ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(220,40),
+                                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 152.0),
+                                backgroundColor: Color(0xff41B06E),
+                                foregroundColor: Color(0xffFFFFFF),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),)
+                                  ),
+                              onPressed: logoutHandle,
+                              child: Text("Logout", style: TextStyle(fontSize: 20.0, color: Color(0xffFFFFFF)),),
+                                )
+                          ],
+                        ),
+                      )                                      
+                    ],
+                  )
+                ) 
+              )
+            ]
+          )                       
+      ),
+    );
+  }
+}
