8342d4e16e343df14177903db1c5cec50f1b3bec
Griemoriez <98567749+Griemoriez@users.noreply.github.com>
2024-06-06 19:46:23 +0700

    INPUT BAHAN BELOM MARI

--- stockin/lib/pages/inputbahanpage.dart
+++ stockin/lib/pages/inputbahanpage.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
@@ -10,15 +14,84 @@ class inputBarangPage extends StatefulWidget {
 
 class _inputBarangPageState extends State<inputBarangPage> {
   List<String> _dropdownItemAsal = ['Gudang', 'Toko'];
-
-  String _selectedAsal = 'Gudang';
-  List<String> _dropdownItemTujuan = ['Proyek A', 'Proyek B', 'Proyek C'];
-
   String _selectedTujuan = 'Proyek A';
-
-  List<String> _dropdownItemBahan = ['Semen', 'Kayu', 'Bata', 'Pasir', 'Beton'];
-
   String _selectedBahan = 'Kayu';
+  List<String> _dropdownItemAsalFiltered = ['Gudang', 'Toko'];
+  String _selectedAsal = 'Gudang';
+  List<String> _dropdownItemTujuan = ['Gudang'];
+  List<String> _dropdownItemBahan = [];
+
+  Future<void> getAllData() async {
+    await getProyek();
+    await getBarang();
+  }
+
+  Future<void> getProyek() async {
+    String getProyek = "http://berkatnusantara.com:5868/proyek";
+
+    try {
+      var req1 = await http.get(Uri.parse(getProyek));
+      if (req1.statusCode == 200) {
+        final data1 = jsonDecode(req1.body);
+        List listGet1 = data1['data'];
+
+        setState(() {
+          _dropdownItemTujuan.clear();
+          for (var index in listGet1) {
+            _dropdownItemTujuan.add(index['nama'].toString());
+          }
+          if (!_dropdownItemTujuan.contains(_selectedTujuan)) {
+            _selectedTujuan =
+                _dropdownItemTujuan.isNotEmpty ? _dropdownItemTujuan[0] : '';
+          }
+        });
+      }
+    } catch (e) {
+      print("Exception caught : $e");
+    }
+  }
+
+  Future<void> getBarang() async {
+    String getBarang = "http://berkatnusantara.com:5868/barang";
+
+    try {
+      var req2 = await http.get(Uri.parse(getBarang));
+      if (req2.statusCode == 200) {
+        final data2 = jsonDecode(req2.body);
+        List listGet2 = data2['data'];
+
+        setState(() {
+          _dropdownItemBahan.clear();
+          for (var index in listGet2) {
+            _dropdownItemBahan.add(index['nama'].toString());
+          }
+          if (!_dropdownItemBahan.contains(_selectedBahan)) {
+            _selectedBahan =
+                _dropdownItemBahan.isNotEmpty ? _dropdownItemBahan[0] : '';
+          }
+        });
+      }
+    } catch (e) {
+      print("Exception caught : $e");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAllData();
+  }
+
+  void _updateAsalDropdown() {
+    if (_selectedTujuan == 'Gudang') {
+      _dropdownItemAsalFiltered = ['Toko'];
+      _selectedAsal = 'Toko';
+    } else {
+      _dropdownItemAsalFiltered = ['Gudang', 'Toko'];
+      _selectedAsal = 'Gudang';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,8 +138,38 @@ class _inputBarangPageState extends State<inputBarangPage> {
                             padding: const EdgeInsets.symmetric(
                                 vertical: 20, horizontal: 15),
                             child: DropdownButtonFormField(
+                              value: _selectedTujuan,
+                              items: _dropdownItemTujuan.map((String item) {
+                                return DropdownMenuItem(
+                                    value: item, child: Text(item));
+                              }).toList(),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _selectedTujuan = value!;
+                                  _updateAsalDropdown();
+                                });
+                              },
+                              icon: Icon(Icons.arrow_drop_down_rounded),
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Color(0xff8DECB4),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      borderSide: BorderSide.none),
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 12),
+                                  labelText: 'Pilih Tujuan',
+                                  labelStyle:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 15),
+                            child: DropdownButtonFormField(
                               value: _selectedAsal,
-                              items: _dropdownItemAsal.map((String item) {
+                              items:
+                                  _dropdownItemAsalFiltered.map((String item) {
                                 return DropdownMenuItem(
                                     value: item, child: Text(item));
                               }).toList(),
@@ -85,34 +188,6 @@ class _inputBarangPageState extends State<inputBarangPage> {
                                   contentPadding: EdgeInsets.symmetric(
                                       horizontal: 16, vertical: 12),
                                   labelText: 'Pilih Asal',
-                                  labelStyle:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 15),
-                            child: DropdownButtonFormField(
-                              value: _selectedTujuan,
-                              items: _dropdownItemTujuan.map((String item) {
-                                return DropdownMenuItem(
-                                    value: item, child: Text(item));
-                              }).toList(),
-                              onChanged: (String? value) {
-                                setState(() {
-                                  _selectedTujuan = value!;
-                                });
-                              },
-                              icon: Icon(Icons.arrow_drop_down_rounded),
-                              decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor: Color(0xff8DECB4),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      borderSide: BorderSide.none),
-                                  contentPadding: EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 12),
-                                  labelText: 'Pilih Tujuan',
                                   labelStyle:
                                       TextStyle(fontWeight: FontWeight.bold)),
                             ),
